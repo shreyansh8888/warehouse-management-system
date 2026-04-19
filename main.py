d346bddda66ff25a00240a0ea8021065131c6b5a
Hitesh <hitesh@example.com>
2026-04-20 01:45:20 +0530

    Refactored main entry point using function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from ui.app import WMSApp
 
-if __name__ == "__main__":
+def main():
+    """Entry point for Warehouse Management System"""
     app = WMSApp()
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
